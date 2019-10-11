af64fcc60711a92ccca74504aea7411697ad9a4c
Aniece Thompson <aniecet@yahoo.com>
2019-10-11 01:57:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") == true
   end
 
   def question?
-
+    self.end_with?("?") == true
   end
 
   def exclamation?
-
+    self.end_with?("!") == true
   end
 
   def count_sentences
-
+    self.split('. ', '? ', '! ')
+    binding.pry
   end
 end
